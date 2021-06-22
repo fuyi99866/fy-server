91f9b9417672dd06cb84be4d20026548f6d42052
yi.fu <yi.fu@ubtrobot.com>
2021-06-22 21:18:49 +0800

    third push

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"go_server/core"
 )
 
@@ -8,6 +9,6 @@ func main() {
 	//global.GVA_VP = core.Viper  //初始化Viper,加载配置到全局配置
 	//global.GVA_LOG = core.Zap() //初始化zap日志库
     //global.GVA_DB = initialize.Gorm() // gorm连接数据库
-	//fmt.Println("start")
+	fmt.Println("start")
 	core.RunWindowsServer()
 }
